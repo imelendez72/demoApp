7dbcd682ea676fa0ebfa966162f99d6d8337cf0b
Israel Melendez <israel.melendez@outlook.com>
2022-10-21 20:01:59 +0000

    Added bootstrap to the files.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 /*
 const jsdom = require("jsdom");
@@ -8,6 +9,19 @@ const jquery = require("jquery")(dom.window);
 */
 app.set("view engine", "pug");
 app.set("views", "./views");
+
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
 
 app.get("/", (req, res) => {
   //res.send("Hello there!");
