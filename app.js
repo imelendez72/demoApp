8ddc33d117ce8e2e2930b84777a95c30505ace7b
Israel Melendez <israel.melendez@outlook.com>
2022-10-21 19:48:15 +0000

    Added pug calls and created 2 pug files.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+/*
+const jsdom = require("jsdom");
+const dom = jsdom.JSDOM();
+const jquery = require("jquery")(dom.window);
+*/
+app.set("view engine", "pug");
+app.set("views", "./views");
 
-app.get('/',(req,res) => {
-    res.send('Hello there!');
+app.get("/", (req, res) => {
+  //res.send("Hello there!");
+  res.render("index");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
